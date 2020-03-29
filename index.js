1051524b28b51a28dadf7d931495b046d42c60f0
blacksonic <soos.gabor86@gmail.com>
2020-03-29 12:12:30 +0200

    feat(server): parsing payloads

--- index.js
+++ index.js
@@ -2,21 +2,31 @@
 
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer((request, response) => {
   const parsedUrl = url.parse(request.url, true);
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  response.end('Hello World\n');
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
-  const requestParameters = {
-    path: trimmedPath,
-    method: request.method.toLowerCase(),
-    query: parsedUrl.query,
-    headers: request.headers
-  };
-  console.log(`Request received: ${JSON.stringify(requestParameters, null, 2)}`);
+  request.on('data', data => buffer += decoder.write(data));
+  request.on('end', () => {
+    buffer += decoder.end();
+
+    response.end('Hello World\n');
+
+    const requestParameters = {
+      path: trimmedPath,
+      method: request.method.toLowerCase(),
+      query: parsedUrl.query,
+      headers: request.headers,
+      payload: buffer
+    };
+    console.log(`Request received: ${JSON.stringify(requestParameters, null, 2)}`);
+  });
 });
 
 const port = process.env.PORT || 3000;
