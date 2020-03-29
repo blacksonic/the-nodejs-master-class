57be0c9577f1a6725b0413b135627d158b5ddfdd
blacksonic <soos.gabor86@gmail.com>
2020-03-29 11:06:23 +0200

    feat(server): parsing request paths

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((request, response) => {
+  const parsedUrl = url.parse(request.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
   response.end('Hello World');
+
+  console.log(`Request received on path: ${trimmedPath}`);
 });
 
 const port = process.env.PORT || 3000;
